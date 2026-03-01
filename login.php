d7d529649b1c05ac7548575631dd27b24cfe6b5e
Kenzie-01 <mby0205@gmail.com>
2026-03-01 12:32:35 -0500

    Add files via upload
    login file with join statements

--- login.php
+++ login.php
@@ -50,11 +50,34 @@ session_start();
         $stmt->close();
     }
     
+    if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+        
+        $naturalJoin = "SELECT * FROM Users NATURAL JOIN Posts LIMIT 10";
+        $con->query($naturalJoin);
+        
+        $innerJoin = "SELECT * FROM Users INNER JOIN Posts ON Users.user_id = Posts.user_id LIMIT 10";
+        $con->query($innerJoin);
+        
+        $leftJoin = "SELECT * FROM Users LEFT JOIN Posts ON Users.user_id = Posts.user_id LIMIT 10";
+        $con->query($leftJoin);
+        
+        $rightJoin = "SELECT * FROM Users RIGHT JOIN Posts ON Users.user_id = Posts.user_id LIMIT 10";
+        $con->query($rightJoin);
+        
+        $fullJoin = "(SELECT * FROM Users LEFT JOIN Posts ON Users.user_id = Posts.user_id) 
+                    UNION 
+                    (SELECT * FROM Users RIGHT JOIN Posts ON Users.user_id = Posts.user_id) 
+                    LIMIT 10";
+        $con->query($fullJoin);
+    }
+    
     $con->close();
     ?>
 
     <p style="color: red;">
         <?php echo $message; ?>
     </p>
+
+    
 </body>
 </html>
